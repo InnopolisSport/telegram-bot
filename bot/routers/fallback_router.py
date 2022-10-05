1f7e5dcbb852f1eb19cc9d4f5c2c63d61b029ad5
Vladimir Makharev <v.makharev@yandex.ru>
2022-10-05 20:42:31 +0300

    Clean the fallback router

--- bot/routers/fallback_router.py
+++ bot/routers/fallback_router.py
@@ -1,21 +1,7 @@
-from typing import Any, Dict
 from aiogram import Router
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
-
-from loguru import logger
-from bot import auth
-from bot.filters import text
-from bot.routers import POLL_NAMES
-from bot.utils import passed_intro_poll, fetch_poll_by_name, API_URL, suggest_training
-
+from aiogram.types import Message
 
 fallback_router = Router()
-
-
-class FallbackStates(StatesGroup):
-    pass
 
 
 @fallback_router.message()
