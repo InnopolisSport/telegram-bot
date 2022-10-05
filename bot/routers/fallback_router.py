e3fd7b5ea60e6fd09e117b62fb1fa4a2ddeacb75
Vladimir Makharev <v.makharev@yandex.ru>
2022-10-06 00:54:13 +0300

    Add logs for fallback router

--- bot/routers/fallback_router.py
+++ bot/routers/fallback_router.py
@@ -1,9 +1,13 @@
 from aiogram import Router
 from aiogram.types import Message
 
+from loguru import logger
+from bot.utils import get_user_string
+
 fallback_router = Router()
 
 
 @fallback_router.message()
 async def fallback(message: Message):
     await message.answer('Любой бот даёт сбой, я не понимаю тебя. Попробуй еще раз')
+    logger.info(f'{get_user_string(message)} fallback: {message.text}')
